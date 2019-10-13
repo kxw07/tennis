b1ae44295a2262e8e8e7b64bbdcfeb7081ddc90a
kai <kai@simbo.com.tw>
2019-10-13 12:04:08 +0800

    add test game_start_then_love_all

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -1,5 +1,11 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TennisTest {
+    @Test
+    public void game_start_then_love_all() {
+        Tennis tennis = new Tennis();
+        Assert.assertEquals(tennis.score(), "Love All");
+    }
 
 }
