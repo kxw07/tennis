7528493c76476a5821818331313a6a33ab0bfb35
kai <kai@simbo.com.tw>
2019-10-13 12:09:21 +0800

    fix test when_a_score_one_then_fifteen_love

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,5 +1,14 @@
 public class Tennis {
+    int a = 0;
+
     public String score() {
+        if (a > 0) {
+            return "Fifteen Love";
+        }
         return "Love All";
+    }
+
+    public void aScore() {
+        a++;
     }
 }
