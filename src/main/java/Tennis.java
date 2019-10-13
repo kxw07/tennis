457518f35d46b2a2b83c105d0553d28bc8cbf16a
kai <kai@simbo.com.tw>
2019-10-13 12:26:45 +0800

    fix test when_a_score_three_then_forty_love

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -21,6 +21,7 @@ public class Tennis {
         scoreMap.put(0, "Love");
         scoreMap.put(1, "Fifteen");
         scoreMap.put(2, "Thirty");
+        scoreMap.put(3, "Forty");
 
         return scoreMap.get(score);
     }
