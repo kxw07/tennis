a084646108fad596190e29369ee933a5e36abc2f
kai <kai@simbo.com.tw>
2019-10-13 12:13:15 +0800

    extract test case initial

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -1,16 +1,22 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisTest {
+    private Tennis tennis;
+
+    @Before
+    public void setup() {
+        tennis = new Tennis();
+    }
+
     @Test
     public void game_start_then_love_all() {
-        Tennis tennis = new Tennis();
         Assert.assertEquals(tennis.score(), "Love All");
     }
 
     @Test
     public void when_a_score_one_then_fifteen_love() {
-        Tennis tennis = new Tennis();
         tennis.aScore();
         Assert.assertEquals(tennis.score(), "Fifteen Love");
     }
