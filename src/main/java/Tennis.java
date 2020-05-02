4e1b3a56341242da1186e6c3663b28b84f9b1d4d
kai <kai@simbo.com.tw>
2020-05-02 14:49:05 +0800

    refactor class tennis

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,11 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tennis {
     private int a_score = 0;
+    Map scoreMap = new HashMap<Integer, String>();
+
+    public Tennis() {
+        scoreMap.put(0, "love");
+        scoreMap.put(1, "fifteen");
+        scoreMap.put(2, "thirty");
+        scoreMap.put(3, "forty");
+    }
 
     public String getScores() {
-        if (a_score == 1) {
-            return "fifteen love";
-        } else if (a_score == 2) {
-            return "thirty love";
+        if (a_score > 0) {
+            return scoreMap.get(a_score) + " " + "love";
         }
 
         return "love all";
