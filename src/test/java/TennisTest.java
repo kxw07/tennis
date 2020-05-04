21628f7479f48e22d1f856ecd9915fa57d7e8307
kai <kai@simbo.com.tw>
2020-05-04 10:02:19 +0800

    Add test case, a normal win

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -72,6 +72,12 @@ public class TennisTest {
         Assert.assertEquals(tennis.getScores(), "win a");
     }
 
+    @Test
+    public void given_a_scores_four_then_return_win_a() {
+        aScoresTimes(4);
+        Assert.assertEquals(tennis.getScores(), "win a");
+    }
+
     public void aScoresTimes(int times) {
         for (int i = 0; i < times; i++) {
             tennis.aScores();
