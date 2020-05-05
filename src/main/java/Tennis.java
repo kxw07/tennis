9668a030d0ed04aade021445af042c7a094c131f
kai <kai@simbo.com.tw>
2020-05-05 09:37:10 +0800

    Fix test case, a scores two

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,10 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tennis {
 
     private int a_score = 0;
 
+    private Map scoreMap;
+
+    public Tennis () {
+        scoreMap = new HashMap();
+        scoreMap.put(0, "love");
+        scoreMap.put(1, "fifteen");
+        scoreMap.put(2, "thirty");
+        scoreMap.put(3, "forty");
+    }
+
     public String getScores() {
         if (a_score > 0) {
-            return "fifteen love";
+            return scoreMap.get(a_score) + " love";
         }
 
         return "love all";
