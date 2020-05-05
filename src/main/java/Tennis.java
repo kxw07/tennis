f60c1a5e1d40cdc58c02d202836971efa08d6636
kai <kai@simbo.com.tw>
2020-05-05 09:57:49 +0800

    Fix test case. normal b win

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -24,14 +24,19 @@ public class Tennis {
                 return scoreMap.get(a_score) + " all";
             }
         } else {
-            if (a_score > 3 || b_score > 3) {
-                if (Math.abs(a_score - b_score) == 2) {
+            if (a_score >= 3 && b_score >= 3) {
+                if (Math.abs(a_score - b_score) == 1) {
+                    return a_score > b_score ? "advantage a":"advantage b";
+                } else {
+                    return a_score > b_score ? "win a":"win b";
+                }
+            } else {
+                if (a_score == 4 || b_score == 4) {
                     return a_score > b_score ? "win a":"win b";
                 } else {
-                    return a_score > b_score ? "advantage a":"advantage b";
+                    return scoreMap.get(a_score) + " " + scoreMap.get(b_score);
                 }
             }
-            return scoreMap.get(a_score) + " " + scoreMap.get(b_score);
         }
     }
 
