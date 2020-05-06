5cb0ef59866e65976913ccd250dac6d8edf4f63d
kai <kai@simbo.com.tw>
2020-05-06 09:46:37 +0800

    Add test case, advantage a

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -13,39 +13,47 @@ public class TennisTest {
 
     @Test
     public void given_init_game_then_return_love_all () {
-        Assert.assertEquals(tennis.getScores(), "love all");
+        Assert.assertEquals("love all", tennis.getScores());
     }
 
     @Test
     public void given_a_scores_one_then_return_fifteen_love () {
         aScoresTimes(1);
-        Assert.assertEquals(tennis.getScores(), "fifteen love");
+        Assert.assertEquals("fifteen love", tennis.getScores());
     }
 
     @Test
     public void given_a_scores_two_then_return_thiry_love () {
         aScoresTimes(2);
-        Assert.assertEquals(tennis.getScores(), "thirty love");
+        Assert.assertEquals("thirty love", tennis.getScores());
     }
 
     @Test
     public void given_b_scores_one_then_return_love_fifteen () {
         bScoresTimes(1);
-        Assert.assertEquals(tennis.getScores(), "love fifteen");
+        Assert.assertEquals("love fifteen", tennis.getScores());
     }
 
     @Test
     public void given_a_and_b_scores_one_then_return_fifteen_all () {
         aScoresTimes(1);
         bScoresTimes(1);
-        Assert.assertEquals(tennis.getScores(), "fifteen all");
+        Assert.assertEquals("fifteen all", tennis.getScores());
     }
 
     @Test
     public void given_a_and_b_scores_three_then_return_deuce () {
         aScoresTimes(3);
         bScoresTimes(3);
-        Assert.assertEquals(tennis.getScores(), "deuce");
+        Assert.assertEquals("deuce", tennis.getScores() );
+    }
+
+    @Test
+    public void given_deuce_when_a_scores_one_then_return_advantage_a () {
+        aScoresTimes(3);
+        bScoresTimes(3);
+        aScoresTimes(1);
+        Assert.assertEquals("advantage a", tennis.getScores());
     }
 
 
