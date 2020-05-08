550767c69ebf2f7e257eaa49a9a8a5c893dcc8bd
kai <kai@simbo.com.tw>
2020-05-08 09:38:04 +0800

    Refactor test

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -1,16 +1,23 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisTest {
+
+    private Tennis tennis;
+
+    @Before
+    public void initTennis() {
+        tennis = new Tennis();
+    }
+
     @Test
     public void given_init_game_then_return_love_all () {
-        Tennis tennis = new Tennis();
         Assert.assertEquals("love all", tennis.getScores());
     }
 
     @Test
     public void given_a_scores_one_then_return_fifteen_love () {
-        Tennis tennis = new Tennis();
         tennis.aScores();
         Assert.assertEquals("fifteen love", tennis.getScores());
     }
