1379e0f01eb4503b3fc170412cd487a1fa7671f2
kai <kai@simbo.com.tw>
2020-05-12 08:49:58 +0800

    Add test case, a scores one

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,5 +1,11 @@
 public class Tennis {
+    private int a_score = 0;
+
     public String getScores() {
         return "love all";
+    }
+
+    public void aScores() {
+        a_score++;
     }
 }
