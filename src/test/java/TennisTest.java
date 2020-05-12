b654bf89c7690331f8a1f7b9e213182051d097f6
kai <kai@simbo.com.tw>
2020-05-12 09:02:16 +0800

    Add test case, deuce

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -41,6 +41,13 @@ public class TennisTest {
         Assert.assertEquals("fifteen all", tennis.getScores());
     }
 
+    @Test
+    public void given_a_and_b_scores_three_then_return_deuce () {
+        aScoresTimes(3);
+        bScoresTimes(3);
+        Assert.assertEquals("deuce", tennis.getScores());
+    }
+
     private void bScoresTimes(int times) {
         for (int i = 0; i < times; i++) {
             tennis.bScores();
