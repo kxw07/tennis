5a95fabb982cee385c6e3a1299284fe2d8cc6f17
kai <kai@simbo.com.tw>
2020-05-14 09:32:27 +0800

    Fix test case, double deuce

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -39,6 +39,10 @@ public class Tennis {
     }
 
     public void bScores() {
-        b_score++;
+        if ((a_score >= 3 && b_score >= 3) && (a_score > b_score)) {
+            a_score--;
+        } else {
+            b_score++;
+        }
     }
 }
