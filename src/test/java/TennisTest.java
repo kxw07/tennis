66bcd79ab0cca113965114556dca6f983fcd128c
kai <kai@simbo.com.tw>
2020-05-17 21:40:38 +0800

    Add test case, fifteen all

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -34,6 +34,13 @@ public class TennisTest {
         Assert.assertEquals("love fifteen", tennis.getScore());
     }
 
+    @Test
+    public void given_a_and_b_scores_one_then_return_fifteen_all() {
+        aScoresTimes(1);
+        bScoresTimes(1);
+        Assert.assertEquals("fifteen all", tennis.getScore());
+    }
+
     private void bScoresTimes(int times) {
         for (int i = 0; i < times; i++) {
             tennis.bScores();
