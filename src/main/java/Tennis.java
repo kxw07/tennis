fd5b6a6c15f709018a523c7a2ad2e6089d2638ca
kai <kai@simbo.com.tw>
2020-05-17 21:30:11 +0800

    Add test case, a scores one

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -2,4 +2,7 @@ public class Tennis {
     public String getScore() {
         return "love all";
     }
+
+    public void aScores() {
+    }
 }
--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -3,8 +3,16 @@ import org.junit.Test;
 
 public class TennisTest {
     @Test
-    public void given_init_game_then_return_love_all () {
+    public void given_init_game_then_return_love_all() {
         Tennis tennis = new Tennis();
         Assert.assertEquals("love all", tennis.getScore());
     }
+
+    @Test
+    public void given_a_scores_one_then_return_fifteen_all() {
+        Tennis tennis = new Tennis();
+        tennis.aScores();
+        Assert.assertEquals("fifteen love", tennis.getScore());
+    }
+
 }
