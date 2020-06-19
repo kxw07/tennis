a19651e6b0eb803421e02bd86d5b24fdec137b3f
kai <kai@simbo.com.tw>
2020-06-19 11:00:07 +0800

    Add test case, win a after advantage a

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -56,6 +56,14 @@ public class TennisTest {
         Assert.assertEquals("advantage a", tennis.getScores());
     }
 
+    @Test
+    public void given_advantage_a_when_a_scores_one_then_return_win_a() {
+        aScoresTimes(3);
+        bScoresTimes(3);
+        aScoresTimes(2);
+        Assert.assertEquals("win a", tennis.getScores());
+    }
+
     private void aScoresTimes(int times) {
         for (int i = 0; i < times; i++) {
             tennis.aScores();
