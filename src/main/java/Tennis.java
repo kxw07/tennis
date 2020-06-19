d4c95b67925d22b3b2f5cbaed6ae101af826273b
kai <kai@simbo.com.tw>
2020-06-19 10:03:55 +0800

    Fix test case, a scores one

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,8 +1,15 @@
 public class Tennis {
+
+    private int aScore = 0;
+
     public String getScores() {
+        if (aScore > 0) {
+            return "fifteen love";
+        }
         return "love all";
     }
 
     public void aScores() {
+        aScore++;
     }
 }
